5a812db5445963be9377357a99d0c72037e179b4
dogukankurban <c.d.kurban007@gmail.com>
2024-09-18 20:45:33 +0300

    ödev2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,14 +10,15 @@ public class Main {
         char[] digits = String.valueOf(number).toCharArray();
         String reversed = "";
         for (int i = digits.length - 1; i >= 0; i--) {
-            reversed += digits[1];
+            reversed = reversed + digits[i];
         }
         return reversed.equalsIgnoreCase(String.valueOf(number));
     }
 
     public static boolean isPerfectNumber(int number) {
-        if (number < 0)
+        if (number < 0) {
             return false;
+        }
 
             int total = 0;
             for (int i = 1; i <= number / 2; i++) {
@@ -33,37 +34,38 @@ public class Main {
             return "Invalid Value";
 
         char[] digits = String.valueOf(number).toCharArray();
-        String numToText = "";
-        for (char digit: digits) {
+        String numToText;
+        numToText = "";
+        for (char digit : digits) {
             switch (digit){
-                case 0:
+                case '0':
                      numToText += "Zero ";
                      break;
-                case 1:
+                case '1':
                     numToText += "One ";
                     break;
-                case 2:
+                case '2':
                     numToText += "Two ";
                     break;
-                case 3:
+                case '3':
                     numToText += "Three ";
                     break;
-                case 4:
+                case '4':
                     numToText += "Four ";
                     break;
-                case 5:
+                case '5':
                     numToText += "Five ";
                     break;
-                case 6:
+                case '6':
                     numToText += "Six ";
                     break;
-                case 7:
+                case '7':
                     numToText += "Seven ";
                     break;
-                case 8:
+                case '8':
                     numToText += "Eight ";
                     break;
-                case 9:
+                case '9':
                     numToText += "Nine ";
                     break;
                 default:
